71677a05355a2d1f53eed86fd155c09df7599382
Rafa <raom30@hotmail.com>
2018-04-18 14:14:31 +0200

    commit 18/04/2018

--- src/main/java/es/aytos/hibernate/hibernate_dual/pruebas/Pruebas.java
+++ src/main/java/es/aytos/hibernate/hibernate_dual/pruebas/Pruebas.java
@@ -1,5 +1,7 @@
 package es.aytos.hibernate.hibernate_dual.pruebas;
 
+import java.util.List;
+
 import es.aytos.hibernate.hibernate_dual.modelo.EstadoCivil;
 import es.aytos.hibernate.hibernate_dual.modelo.Persona;
 import es.aytos.hibernate.hibernate_dual.repositorio.RepositorioPersona;
@@ -7,8 +9,10 @@ import es.aytos.hibernate.hibernate_dual.repositorio.RepositorioPersona;
 public class Pruebas {
 
 	public static void main(String[] args) {
-		System.out.println(crearPersona());
-
+		//System.out.println(crearPersona());
+		//modificarPersona();
+		//eliminarPersona(1);
+		consultarPersona(7);
 	}
 
 	private static Integer crearPersona() {
@@ -20,5 +24,26 @@ public class Pruebas {
 		persona.setDni("15404157E");
 		return RepositorioPersona.crearPersona(persona);
 	}
+	
+	private static void modificarPersona() {
+		RepositorioPersona.modifiarPersona(1, "PEPE");
+	}
+	
+	private static void eliminarPersona(Integer idPersona) {
+		RepositorioPersona.eliminarPersona(idPersona);
+	}
+	public static void consultarPersona(Integer idPersona) {
+		Persona persona = RepositorioPersona.consultarPersona(idPersona);
+		System.out.println(persona.getNombre());
+		System.out.println(persona.getApellidos());
+		System.out.println(persona.getDni());
+		System.out.println(persona.getEstadoCivil());
+		
+	}
+	public static void consultar(String nombre, String apellidos, String dni, EstadoCivil estadoCivil) {
+		List<Persona> personas = RepositorioPersona.consultar(nombre, apellidos, dni, estadoCivil);
+		System.out.println(personas.size());
+	}
+
 
 }
--- src/main/java/es/aytos/hibernate/hibernate_dual/repositorio/RepositorioPersona.java
+++ src/main/java/es/aytos/hibernate/hibernate_dual/repositorio/RepositorioPersona.java
@@ -1,7 +1,11 @@
 package es.aytos.hibernate.hibernate_dual.repositorio;
 
-import org.hibernate.Session;
+import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import es.aytos.hibernate.hibernate_dual.modelo.EstadoCivil;
 import es.aytos.hibernate.hibernate_dual.modelo.Persona;
 import es.aytos.hibernate.hibernate_dual.util.HibernateUtil;
 
@@ -39,6 +43,9 @@ public class RepositorioPersona {
 			final Persona personaBBDD = (Persona) sesion.createQuery("from Persona where PER_ID = :idPersona")
 					.setParameter("idPersona", idPersona).uniqueResult();// Almacena esta persona
 			
+			/*sesion.createQuery("UPDATE Persona set per_nom = :nombre where per_id = :identificador")
+			.setParameter("nombre", nombre).setParameter("identificador", idPersona).uniqueResult();*/
+			
 			personaBBDD.setNombre(nombre);
 
 			sesion.getTransaction().commit(); // Lo guarda en la base de datos
@@ -52,4 +59,95 @@ public class RepositorioPersona {
 			sesion.close();
 		}
 	}
+	
+	public static void eliminarPersona(Integer idPersona) {
+		final Session sesion = HibernateUtil.getMiFactoria().getCurrentSession();
+
+		try {
+
+			sesion.beginTransaction();
+			
+			sesion.createQuery("DELETE Persona where per_id = :identificador")
+			.setParameter("identificador", idPersona).executeUpdate();
+
+			sesion.getTransaction().commit(); // Lo guarda en la base de datos
+
+
+		} catch (Exception e) {
+			System.out.println("Se ha producido un error insertando la persona: " + e);
+			e.printStackTrace();
+			throw new RuntimeException();
+		}finally {
+			sesion.close();
+		}
+	}
+	public static Persona consultarPersona(Integer idPersona) {
+		final Session sesion = HibernateUtil.getMiFactoria().getCurrentSession();
+
+		try {
+
+			sesion.beginTransaction();
+			
+			return (Persona)sesion.createQuery("from Persona where per_id = :idPersona").setParameter("idPersona", idPersona).uniqueResult();
+
+
+
+		} catch (Exception e) {
+			System.out.println("Se ha producido un error insertando la persona: " + e);
+			e.printStackTrace();
+			throw new RuntimeException();
+		}finally {
+			sesion.close();
+		}
+	}
+	public static List<Persona> consultar(String nombre, String apellidos, String dni, EstadoCivil estadoCivil) {
+		final Session sesion = HibernateUtil.getMiFactoria().getCurrentSession();
+
+		try {
+
+			sesion.beginTransaction();
+			
+			final StringBuilder sb = new StringBuilder("from Persona Where 1=1");
+			if(!nombre.isEmpty()) {
+				sb.append("and Per_NOM LIKE :nombre");
+			}
+			if(!apellidos.isEmpty()) {
+				sb.append("and PER_APE like :apellidos");
+			}
+			if(!dni.isEmpty()) {
+				sb.append("AND PER_DNI :dni");
+			}
+			if(estadoCivil != null) {
+				sb.append("AND PER_ECV = :estadoCivil");
+			}
+			
+			final Query<Persona> consulta = sesion.createQuery(sb.toString());
+			
+			if(!nombre.isEmpty()) {
+				consulta.setParameter("nombre", nombre);
+			}
+			if(!apellidos.isEmpty()) {
+				consulta.setParameter("apellidos", apellidos);
+			}
+			if(!dni.isEmpty()) {
+				consulta.setParameter("dni", dni);
+			}
+			if(estadoCivil != null) {
+				consulta.setParameter("estadoCivil", estadoCivil);
+			}
+			
+			
+
+			return consulta.list();
+
+
+		} catch (Exception e) {
+			System.out.println("Se ha producido un error insertando la persona: " + e);
+			e.printStackTrace();
+			throw new RuntimeException();
+		}finally {
+			sesion.close();
+		}
+	}
+	
 }
